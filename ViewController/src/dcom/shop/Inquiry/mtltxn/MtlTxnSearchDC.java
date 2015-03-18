9aa1661e0f1c02ba3c29be86536f2f0e54222e07
ashishgupta83 <ashishgupta83@deloitte.com>
2015-03-18 21:07:20 +0530

    added DC

---
+++ ViewController/src/dcom/shop/Inquiry/mtltxn/MtlTxnSearchDC.java
@@ -0,0 +1,174 @@
+package dcom.shop.Inquiry.mtltxn;
+
+import dcom.shop.restURIDetails.RestURI;
+
+import dcom.shop.restURIDetails.RestCallerUtil;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import java.util.logging.Level;
+
+import javax.el.ValueExpression;
+
+import javax.xml.transform.SourceLocator;
+
+import oracle.adfmf.framework.api.AdfmfJavaUtilities;
+import oracle.adfmf.framework.api.JSONBeanSerializationHelper;
+
+import oracle.adfmf.util.Utility;
+import oracle.adfmf.util.logging.Trace;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class MtlTxnSearchDC {
+    public MtlTxnSearchDC() {
+        super();
+    }
+    List s_mtlTxnList = new ArrayList();
+
+    public MtlTxnSearchEntity[] getAllmtlTxn() {
+        ValueExpression ve = null;
+
+        System.out.println("Inside mtl txn search");
+        Utility.ApplicationLogger.info("Inside script dcomShopFloor.db");
+        String item = null;
+        String subinv = null;
+        String locator = null;
+        String lpn = null;
+        String txnType = null;
+
+        ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.searchKeyword}", String.class);
+        item = ((String) ve.getValue(AdfmfJavaUtilities.getAdfELContext())).trim();
+
+        ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.FromSubinventory}", String.class);
+        subinv = ((String) ve.getValue(AdfmfJavaUtilities.getAdfELContext())).trim();
+
+        ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.FromLocator}", String.class);
+        locator = ((String) ve.getValue(AdfmfJavaUtilities.getAdfELContext())).trim();
+
+        ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.searchLpnKeyword}", String.class);
+        lpn = ((String) ve.getValue(AdfmfJavaUtilities.getAdfELContext())).trim();
+
+        ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.txnType}", String.class);
+        txnType = ((String) ve.getValue(AdfmfJavaUtilities.getAdfELContext())).trim();
+
+        String restURI = RestURI.PostMtlTxnInquiryURI();
+        RestCallerUtil rcu = new RestCallerUtil();
+        String payload =
+            "{\"x\":\n" + "{\n" + "   \"RESTHeader\": {\"Responsibility\": \"ORDER_MGMT_SUPER_USER\",\n" +
+            "                  \"RespApplication\": \"ONT\",\n" +
+            "                  \"SecurityGroup\": \"STANDARD\",\n" +
+            "                  \"NLSLanguage\": \"AMERICAN\",\n" + "                  \"Org_Id\": \"82\"\n" +
+            "                 },\n" + "   \"InputParameters\": \n" + "     {\"PMTLTXNREQ\": {\"TRXTYPE\": \"" +
+            txnType + "\",\"ORGCODE\": \"100\",\"ITEM\": \"" + item + "\",\"SUBINV\": \"" + subinv +
+            "\",\"LOCATOR\": \"" + locator + "\",\"LPN\": \"" + lpn + "\"}}\n" + "}\n" + "}";
+
+        System.out.println("Calling create method");
+        String jsonArrayAsString = (rcu.invokeUPDATE(restURI, payload)).toString();
+        System.out.println("Received response");
+        MtlTxnSearchEntity[] mtlTxnArray = null;
+        //ashish
+        try {
+            JSONParser parser = new JSONParser();
+            Object object = parser.parse(jsonArrayAsString);
+            JSONObject jsonObject = (JSONObject) object;
+            JSONObject jsObject = (JSONObject) jsonObject.get("OutputParameters");
+            JSONObject jsObject1 = (JSONObject) jsObject.get("XMTLTXNRES");
+            try {
+                JSONArray array = (JSONArray) jsObject1.get("XMTLTXNRES_ITEM");
+
+                if (array != null) {
+                    int size = array.size();
+                    //  ProductSearchEntity[] prodItems= new ProductSearchEntity[size];
+                    for (int i = 0; i < size; i++) {
+
+
+                        MtlTxnSearchEntity mtlTxnItems = new MtlTxnSearchEntity();
+                        //  JSONObject jsObjectArrayData = (JSONObject) jsObject1.get("XLPNRES_ITEM");
+                        JSONObject jsObjectArrayData = (JSONObject) array.get(i);
+                      
+                        mtlTxnItems.setITEM((jsObjectArrayData.get("ITEM").toString()));
+                        mtlTxnItems.setSOURCSUBINV((jsObjectArrayData.get("SOURCSUBINV").toString()));
+                        mtlTxnItems.setSOURCELOCATOR((jsObjectArrayData.get("SOURCELOCATOR").toString()));
+                        mtlTxnItems.setTRXTYPE((jsObjectArrayData.get("TRXTYPE").toString()));
+                        mtlTxnItems.setTRXREF(Integer.parseInt((jsObjectArrayData.get("TRXREF").toString())));
+                        mtlTxnItems.setTRXQTY(Integer.parseInt((jsObjectArrayData.get("TRXQTY").toString())));
+                        mtlTxnItems.setTXNUOM((jsObjectArrayData.get("TXNUOM").toString()));
+                        
+                        String txnDateStr = ((jsObjectArrayData.get("TRXDATE").toString())).substring(0, 10);
+                        try{
+                            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+                            Date date = formatter.parse(txnDateStr);
+                            System.out.println(date);
+                            System.out.println(formatter.format(date));
+                            SimpleDateFormat formatter1 = new SimpleDateFormat("dd-mm-yyyy");
+                            Date date1 = formatter1.parse(txnDateStr);
+                            System.out.println(formatter1.format(date1));
+                            mtlTxnItems.setTRXDATE(date1);
+                        }
+                        catch (ParseException e) {
+                                e.printStackTrace();
+                        }
+                        
+
+                        //   JSONObject onHandOptObj = (JSONObject) jsObject1.get("XONHANDRES_ITEM");
+                        //    JSONObject costGrpObj = (JSONObject) onHandOptObj.get("XONHANDRES_ITEM");
+                        //   onhandItems.setCOSTGROUP((costGrpObj.get("COSTGROUP").toString()));
+
+
+                        s_mtlTxnList.add(mtlTxnItems);
+                    }
+
+
+                }
+            } catch (ClassCastException e2) {
+                JSONObject jsObject2 = (JSONObject) jsObject1.get("XMTLTXNRES_ITEM");
+                if (jsObject2 != null) {
+
+
+                    MtlTxnSearchEntity mtlTxnItems = new MtlTxnSearchEntity();
+                    // JSONObject jsObject2 = (JSONObject)
+
+                    mtlTxnItems.setITEM((jsObject2.get("ITEM").toString()));
+                    mtlTxnItems.setSOURCSUBINV((jsObject2.get("SOURCSUBINV").toString()));
+                    mtlTxnItems.setSOURCELOCATOR((jsObject2.get("SOURCELOCATOR").toString()));
+                    mtlTxnItems.setTRXTYPE((jsObject2.get("TRXTYPE").toString()));
+                    mtlTxnItems.setTRXREF(Integer.parseInt((jsObject2.get("TRXREF").toString())));
+                    mtlTxnItems.setTRXQTY(Integer.parseInt((jsObject2.get("TRXQTY").toString())));
+                    mtlTxnItems.setTXNUOM((jsObject2.get("TXNUOM").toString()));
+                    String txnDateStr = ((jsObject2.get("TRXDATE").toString())).substring(0, 10);
+                    try{
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+                        Date date = formatter.parse(txnDateStr);
+                        System.out.println(date);
+                        System.out.println(formatter.format(date));
+                        SimpleDateFormat formatter1 = new SimpleDateFormat("dd-mm-yyyy");
+                        Date date1 = formatter1.parse(txnDateStr);
+                        System.out.println(formatter1.format(date1));
+                        mtlTxnItems.setTRXDATE(date1);
+                    }
+                    catch (ParseException e) {
+                            e.printStackTrace();
+                    }
+
+                   s_mtlTxnList.add(mtlTxnItems);
+
+
+                }
+            }
+
+            mtlTxnArray = (MtlTxnSearchEntity[]) s_mtlTxnList.toArray(new MtlTxnSearchEntity[s_mtlTxnList.size()]);
+
+        } catch (Exception e) {
+            Trace.log("REST_JSON", Level.SEVERE, this.getClass(), "MtlTxnSearchEntity", e.getLocalizedMessage());
+        }
+        return mtlTxnArray;
+    }
+}
---
+++ ViewController/src/dcom/shop/Inquiry/mtltxn/MtlTxnSearchEntity.java
@@ -0,0 +1,275 @@
+package dcom.shop.Inquiry.mtltxn;
+
+import java.util.Date;
+
+import oracle.adfmf.java.beans.PropertyChangeListener;
+import oracle.adfmf.java.beans.PropertyChangeSupport;
+
+public class MtlTxnSearchEntity {
+    private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public MtlTxnSearchEntity() {
+        super();
+    }
+    private int TRXREF;
+
+    public void setTRXREF(int TRXREF) {
+        int oldTRXREF = this.TRXREF;
+        this.TRXREF = TRXREF;
+        propertyChangeSupport.firePropertyChange("TRXREF", oldTRXREF, TRXREF);
+    }
+
+    public int getTRXREF() {
+        return TRXREF;
+    }
+
+    public void setSOURCETYPE(String SOURCETYPE) {
+        String oldSOURCETYPE = this.SOURCETYPE;
+        this.SOURCETYPE = SOURCETYPE;
+        propertyChangeSupport.firePropertyChange("SOURCETYPE", oldSOURCETYPE, SOURCETYPE);
+    }
+
+    public String getSOURCETYPE() {
+        return SOURCETYPE;
+    }
+
+    public void setTRXTYPE(String TRXTYPE) {
+        String oldTRXTYPE = this.TRXTYPE;
+        this.TRXTYPE = TRXTYPE;
+        propertyChangeSupport.firePropertyChange("TRXTYPE", oldTRXTYPE, TRXTYPE);
+    }
+
+    public String getTRXTYPE() {
+        return TRXTYPE;
+    }
+
+    public void setTRXDATE(Date TRXDATE) {
+        Date oldTRXDATE = this.TRXDATE;
+        this.TRXDATE = TRXDATE;
+        propertyChangeSupport.firePropertyChange("TRXDATE", oldTRXDATE, TRXDATE);
+    }
+
+    public Date getTRXDATE() {
+        return TRXDATE;
+    }
+
+    public void setSOURCEORGCODE(String SOURCEORGCODE) {
+        String oldSOURCEORGCODE = this.SOURCEORGCODE;
+        this.SOURCEORGCODE = SOURCEORGCODE;
+        propertyChangeSupport.firePropertyChange("SOURCEORGCODE", oldSOURCEORGCODE, SOURCEORGCODE);
+    }
+
+    public String getSOURCEORGCODE() {
+        return SOURCEORGCODE;
+    }
+
+    public void setITEM(String ITEM) {
+        String oldITEM = this.ITEM;
+        this.ITEM = ITEM;
+        propertyChangeSupport.firePropertyChange("ITEM", oldITEM, ITEM);
+    }
+
+    public String getITEM() {
+        return ITEM;
+    }
+
+    public void setSOURCSUBINV(String SOURCSUBINV) {
+        String oldSOURCSUBINV = this.SOURCSUBINV;
+        this.SOURCSUBINV = SOURCSUBINV;
+        propertyChangeSupport.firePropertyChange("SOURCSUBINV", oldSOURCSUBINV, SOURCSUBINV);
+    }
+
+    public String getSOURCSUBINV() {
+        return SOURCSUBINV;
+    }
+
+    public void setSOURCELOCATOR(String SOURCELOCATOR) {
+        String oldSOURCELOCATOR = this.SOURCELOCATOR;
+        this.SOURCELOCATOR = SOURCELOCATOR;
+        propertyChangeSupport.firePropertyChange("SOURCELOCATOR", oldSOURCELOCATOR, SOURCELOCATOR);
+    }
+
+    public String getSOURCELOCATOR() {
+        return SOURCELOCATOR;
+    }
+
+    public void setLPN(String LPN) {
+        String oldLPN = this.LPN;
+        this.LPN = LPN;
+        propertyChangeSupport.firePropertyChange("LPN", oldLPN, LPN);
+    }
+
+    public String getLPN() {
+        return LPN;
+    }
+
+    public void setDESTORG(String DESTORG) {
+        String oldDESTORG = this.DESTORG;
+        this.DESTORG = DESTORG;
+        propertyChangeSupport.firePropertyChange("DESTORG", oldDESTORG, DESTORG);
+    }
+
+    public String getDESTORG() {
+        return DESTORG;
+    }
+
+    public void setDESTSUBINV(String DESTSUBINV) {
+        String oldDESTSUBINV = this.DESTSUBINV;
+        this.DESTSUBINV = DESTSUBINV;
+        propertyChangeSupport.firePropertyChange("DESTSUBINV", oldDESTSUBINV, DESTSUBINV);
+    }
+
+    public String getDESTSUBINV() {
+        return DESTSUBINV;
+    }
+
+    public void setDESTLOCATOR(String DESTLOCATOR) {
+        String oldDESTLOCATOR = this.DESTLOCATOR;
+        this.DESTLOCATOR = DESTLOCATOR;
+        propertyChangeSupport.firePropertyChange("DESTLOCATOR", oldDESTLOCATOR, DESTLOCATOR);
+    }
+
+    public String getDESTLOCATOR() {
+        return DESTLOCATOR;
+    }
+
+    public void setSOURCECODE(String SOURCECODE) {
+        String oldSOURCECODE = this.SOURCECODE;
+        this.SOURCECODE = SOURCECODE;
+        propertyChangeSupport.firePropertyChange("SOURCECODE", oldSOURCECODE, SOURCECODE);
+    }
+
+    public String getSOURCECODE() {
+        return SOURCECODE;
+    }
+
+    public void setCOSTED(String COSTED) {
+        String oldCOSTED = this.COSTED;
+        this.COSTED = COSTED;
+        propertyChangeSupport.firePropertyChange("COSTED", oldCOSTED, COSTED);
+    }
+
+    public String getCOSTED() {
+        return COSTED;
+    }
+
+    public void setTRXQTY(int TRXQTY) {
+        int oldTRXQTY = this.TRXQTY;
+        this.TRXQTY = TRXQTY;
+        propertyChangeSupport.firePropertyChange("TRXQTY", oldTRXQTY, TRXQTY);
+    }
+
+    public int getTRXQTY() {
+        return TRXQTY;
+    }
+
+    public void setTXNUOM(String TXNUOM) {
+        String oldTXNUOM = this.TXNUOM;
+        this.TXNUOM = TXNUOM;
+        propertyChangeSupport.firePropertyChange("TXNUOM", oldTXNUOM, TXNUOM);
+    }
+
+    public String getTXNUOM() {
+        return TXNUOM;
+    }
+
+    public void setGLACCOUNT(String GLACCOUNT) {
+        String oldGLACCOUNT = this.GLACCOUNT;
+        this.GLACCOUNT = GLACCOUNT;
+        propertyChangeSupport.firePropertyChange("GLACCOUNT", oldGLACCOUNT, GLACCOUNT);
+    }
+
+    public String getGLACCOUNT() {
+        return GLACCOUNT;
+    }
+
+    public void setACCOUNTALIAS(String ACCOUNTALIAS) {
+        String oldACCOUNTALIAS = this.ACCOUNTALIAS;
+        this.ACCOUNTALIAS = ACCOUNTALIAS;
+        propertyChangeSupport.firePropertyChange("ACCOUNTALIAS", oldACCOUNTALIAS, ACCOUNTALIAS);
+    }
+
+    public String getACCOUNTALIAS() {
+        return ACCOUNTALIAS;
+    }
+
+    public void setREASON(String REASON) {
+        String oldREASON = this.REASON;
+        this.REASON = REASON;
+        propertyChangeSupport.firePropertyChange("REASON", oldREASON, REASON);
+    }
+
+    public String getREASON() {
+        return REASON;
+    }
+
+    public void setTRXNACTION(String TRXNACTION) {
+        String oldTRXNACTION = this.TRXNACTION;
+        this.TRXNACTION = TRXNACTION;
+        propertyChangeSupport.firePropertyChange("TRXNACTION", oldTRXNACTION, TRXNACTION);
+    }
+
+    public String getTRXNACTION() {
+        return TRXNACTION;
+    }
+
+    public void setTRXSOURCE(String TRXSOURCE) {
+        String oldTRXSOURCE = this.TRXSOURCE;
+        this.TRXSOURCE = TRXSOURCE;
+        propertyChangeSupport.firePropertyChange("TRXSOURCE", oldTRXSOURCE, TRXSOURCE);
+    }
+
+    public String getTRXSOURCE() {
+        return TRXSOURCE;
+    }
+
+    public void setDEPT(String DEPT) {
+        String oldDEPT = this.DEPT;
+        this.DEPT = DEPT;
+        propertyChangeSupport.firePropertyChange("DEPT", oldDEPT, DEPT);
+    }
+
+    public String getDEPT() {
+        return DEPT;
+    }
+
+    public void setCREATEDBY(String CREATEDBY) {
+        String oldCREATEDBY = this.CREATEDBY;
+        this.CREATEDBY = CREATEDBY;
+        propertyChangeSupport.firePropertyChange("CREATEDBY", oldCREATEDBY, CREATEDBY);
+    }
+
+    public String getCREATEDBY() {
+        return CREATEDBY;
+    }
+    private String SOURCETYPE;
+    private String TRXTYPE;
+    private Date TRXDATE;
+    private String SOURCEORGCODE;
+    private String ITEM;
+    private String SOURCSUBINV;
+    private String SOURCELOCATOR;
+    private String LPN;
+    private String DESTORG;
+    private String DESTSUBINV;
+    private String DESTLOCATOR;
+    private String SOURCECODE;
+    private String COSTED;
+    private int TRXQTY;
+    private String TXNUOM;
+    private String GLACCOUNT;
+    private String ACCOUNTALIAS;
+    private String REASON;
+    private String TRXNACTION;
+    private String TRXSOURCE;
+    private String DEPT;
+    private String CREATEDBY;
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.removePropertyChangeListener(l);
+    }
+}
