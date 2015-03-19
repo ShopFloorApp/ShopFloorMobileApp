5e953abc50cce2b6c7262e6e8c3b3e2f2c9638ba
Diprish <diprish@gmail.com>
2015-03-19 14:43:04 +0530

    Dispatch Pages

--- ViewController/src/dcom/shop/application/dc/dispatch/TransactDC.java
+++ ViewController/src/dcom/shop/application/dc/dispatch/TransactDC.java
@@ -1,6 +1,11 @@
 package dcom.shop.application.dc.dispatch;
 
 import dcom.shop.application.mobile.dispatch.TransactBO;
+import dcom.shop.restURIDetails.RestCallerUtil;
+import dcom.shop.restURIDetails.RestURI;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class TransactDC {
     public TransactDC() {
@@ -16,18 +21,86 @@ public class TransactDC {
     
     public String saveTransaction(TransactBO transactBo){
         StringBuffer strPayload = new StringBuffer();
-        /*
-        strPayload.append("{\n" + "  \"x\": {\n" + "    \"RESTHeader\": {\n" +
-                          "      \"Responsibility\": \"ORDER_MGMT_SUPER_USER\",\n" +
-                          "      \"RespApplication\": \"ONT\",\n" + "      \"SecurityGroup\": \"STANDARD\",\n" +
-                          "      \"NLSLanguage\": \"AMERICAN\",\n" + "      \"Org_Id\": \"82\"\n" + "    },\n" +
-                          "    \"InputParameters\": {\n" + "      \"PORGCODE\": \"" + orgCode + "\",\n" +
-                          "      \"PDEPTCODE\": \"" + deptCode + "\",\n" + "      \"PJOB\": \"" + jobNumber + "\"\n" +
-                          "    }\n" + "  }\n" + "}");
-
-
+        RestCallerUtil restCallerUtil = new RestCallerUtil();
+        
+        strPayload.append("{\n" + 
+        "  \"x\": {\n" + 
+        "    \"RESTHeader\": {\n" + 
+        "      \"Responsibility\": \"ORDER_MGMT_SUPER_USER\",\n" + 
+        "      \"RespApplication\": \"ONT\",\n" + 
+        "      \"SecurityGroup\": \"STANDARD\",\n" + 
+        "      \"NLSLanguage\": \"AMERICAN\",\n" + 
+        "      \"Org_Id\": \"82\"\n" + 
+        "    },\n" + 
+        "    \"InputParameters\": {\n" + 
+        "      \"PWIPTXN\": {\n" + 
+        "        \"APPS.XXDCOM_WIP_TXN_IN\": {\n" + 
+        "          \"ACTION\": \""+transactBo.getAction()+"\",\n" + 
+        "          \"TRXREF\": \""+transactBo.getTrxRef()+"\",\n" + 
+        "          \"SOURCETYPE\": \""+transactBo.getSourceType()+"\",\n" + 
+        "          \"TRXTYPE\": \""+transactBo.getTrxType()+"\",\n" + 
+        "          \"TRXDATE\": \""+transactBo.getTrxDate()+"\",\n" + 
+        "          \"ORGCODE\": \""+transactBo.getOrgCode()+"\",\n" + 
+        "          \"ITEM\": \""+transactBo.getItem()+"\",\n" + 
+        "          \"SUBINV\": \""+transactBo.getSubinv()+"\",\n" + 
+        "          \"LOCATOR\": \""+transactBo.getLocator()+"\",\n" + 
+        "          \"LPN\": \""+transactBo.getLpn()+"\",\n" + 
+        "          \"SOURCECODE\": \""+transactBo.getSourceCode()+"\",\n" + 
+        "          \"TRXQTY\": \""+transactBo.getTrxQty()+"\",\n" + 
+        "          \"TXNUOM\": \""+transactBo.getTxnUom()+"\",\n" + 
+        "          \"GLACCOUNT\": \""+transactBo.getGlAccount()+"\",\n" + 
+        "          \"ACCOUNTALIAS\": \""+transactBo.getAccountAlias()+"\",\n" + 
+        "          \"REASON\": \""+transactBo.getReason()+"\",\n" + 
+        "          \"TRXNACTION\": \""+transactBo.getTrxnAction()+"\",\n" + 
+        "          \"TRXSOURCE\": \""+transactBo.getTrxSource()+"\",\n" + 
+        "          \"WIPENTITYNAME\": \""+transactBo.getWipEntityName()+"\",\n" + 
+        "          \"DEPT\": \""+transactBo.getDept()+"\",\n" + 
+        "          \"FROMOPSEQ\": \""+transactBo.getFromOpSeq()+"\",\n" + 
+        "          \"TOOPSEQ\": \""+transactBo.getToOpSeq()+"\",\n" + 
+        "          \"CREATEDBY\": \""+transactBo.getCreatedBy()+"\",\n" + 
+        "          \"SALESORDER\": \""+transactBo.getSalesOrder()+"\",\n" + 
+        "          \"NOTES\": \""+transactBo.getNotes()+"\",\n" + 
+        "          \"REFERENCE\": \""+transactBo.getReference()+"\",\n" + 
+        "          \"KANBAN\": \""+transactBo.getKanban()+"\",\n" + 
+        "          \"ATTRIB\": \"\",\n" + 
+        "          \"LOTS\": {\n" + 
+        "            \"APPS.XXDCOM_LOT_TAB\": {\n" + 
+        "              \"LOTS_ITEM\": {\n" + 
+        "                \"APPS.XXDCOM_LOT_TYPE\": {\n" + 
+        "                  \"LOT\": \"\",\n" + 
+        "                  \"LOTQTY\": \"\"\n" + 
+        "                }\n" + 
+        "              }\n" + 
+        "            }\n" + 
+        "          },\n" + 
+        "          \"SERIALS\": {\n" + 
+        "            \"SERIALS_ITEM\": {\n" + 
+        "              \"APPS.XXDCOM_SERIAL_TYPE\": {\n" + 
+        "                \"FROMSERIAL\": \"\",\n" + 
+        "                \"TOSERIAL\": \"\",\n" + 
+        "                \"SERIALQTY\": \"\"\n" + 
+        "              }\n" + 
+        "            }\n" + 
+        "          }\n" + 
+        "        }\n" + 
+        "      }\n" + 
+        "    }\n" + 
+        "  }\n" + 
+        "}");
         String jsonArrayAsString = restCallerUtil.invokeUPDATE(RestURI.PostGetJobProp(), strPayload.toString());
-        */
+        if (jsonArrayAsString != null) {
+            try {
+                JSONParser parser = new JSONParser();
+                Object object;
+                object = parser.parse(jsonArrayAsString);
+                JSONObject jsonObject = (JSONObject) object;
+                JSONObject jsObject = (JSONObject) jsonObject.get("OutputParameters");
+                JSONObject jsObject1 = (JSONObject) jsObject.get("XSTATUS");
+                return jsObject1.toString();
+            } catch (Exception e) {
+                e.getMessage();
+            }
+        }
         return null;
     }
 }
