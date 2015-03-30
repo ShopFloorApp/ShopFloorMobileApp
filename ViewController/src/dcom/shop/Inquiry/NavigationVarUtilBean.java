f71793052f28993cb036b49c73aa82157733bfcf
ashishgupta83 <ashishgupta83@deloitte.com>
2015-03-30 22:05:04 +0530

    updated

---
+++ ViewController/src/dcom/shop/Inquiry/NavigationVarUtilBean.java
@@ -0,0 +1,23 @@
+package dcom.shop.Inquiry;
+
+import javax.el.ValueExpression;
+
+import oracle.adfmf.framework.api.AdfmfJavaUtilities;
+
+public class NavigationVarUtilBean {
+    public NavigationVarUtilBean() {
+        super();
+    }
+    public String getOnhandNavigations(){
+        ValueExpression ve = null;
+        String callingPage = null;
+        ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.callingPg}", String.class);
+        callingPage = ((String) ve.getValue(AdfmfJavaUtilities.getAdfELContext())).trim();
+        
+        if (callingPage.equals("prodInq")){
+            return "prodOnhandListBack";
+        }
+        else 
+            return "__back";
+    }
+}
