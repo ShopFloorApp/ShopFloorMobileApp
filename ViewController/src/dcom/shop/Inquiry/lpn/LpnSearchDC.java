cdb2f293e2fa713a0540480d6a6579c3fa849126
Diprish <diprish@gmail.com>
2015-04-15 17:44:54 +0530

    Fix DataBinding.cpx issues

--- ViewController/src/dcom/shop/Inquiry/lpn/LpnSearchDC.java
+++ ViewController/src/dcom/shop/Inquiry/lpn/LpnSearchDC.java
@@ -5,12 +5,12 @@ import dcom.shop.restURIDetails.RestURI;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 
 import javax.el.ValueExpression;
 
 import oracle.adfmf.framework.api.AdfmfJavaUtilities;
+import oracle.adfmf.framework.exception.NullPointerException;
 import oracle.adfmf.java.beans.PropertyChangeListener;
 import oracle.adfmf.java.beans.PropertyChangeSupport;
 import oracle.adfmf.java.beans.ProviderChangeListener;
@@ -31,91 +31,78 @@ public class LpnSearchDC {
     private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
     public LpnSearchEntity[] getAllLpns() {
-        ValueExpression ve = null;
-        s_LpnList.clear();
-        Map pageFlow = (Map) AdfmfJavaUtilities.evaluateELExpression("#{pageFlowScope}");
-        pageFlow.put("keywrdLpnLenErr", "");
-        
-        System.out.println("Inside lpn search");
-        Utility.ApplicationLogger.info("Inside script dcomShopFloor.db");
-        String keyword = null;
-        ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.searchLpnKeyword}", String.class);
-        keyword = (String) ve.getValue(AdfmfJavaUtilities.getAdfELContext());
-        String orgCode = (String)AdfmfJavaUtilities.evaluateELExpression("#{preferenceScope.feature.dcom.shop.MyWarehouse.OrgCodePG.OrgCode}");
-        
-        String restURI = RestURI.PostLpnDetailsURI();
-        RestCallerUtil rcu = new RestCallerUtil();
-        String payload =
-            "{\"x\": {\"RESTHeader\": {\"@xmlns\": \"http://xmlns.oracle.com/apps/inv/rest/DCOMInquiry/header\",\"Responsibility\": \"ORDER_MGMT_SUPER_USER\",\"RespApplication\": \"ONT\",\"SecurityGroup\": \"STANDARD\",\n" +
-            "\"NLSLanguage\": \"AMERICAN\",\"Org_Id\": \"82\"},\"InputParameters\": {\"PLPNREQ\": {\"ORGCODE\": \""+orgCode+"\",\"LPN\": \"%" +
-            keyword + "%\"}}\n" + "}\n" + "}";
-        System.out.println("Calling create method");
-        String jsonArrayAsString = (rcu.invokeUPDATE(restURI, payload)).toString();
-        System.out.println("Received response");
-        LpnSearchEntity[] lpnArray = null;
-        //ashish
+        String backFrom = null;
         try {
-            JSONParser parser = new JSONParser();
-            Object object = parser.parse(jsonArrayAsString);
-            JSONObject jsonObject = (JSONObject) object;
-            JSONObject jsObject = (JSONObject) jsonObject.get("OutputParameters");
-            JSONObject jsObject1 = (JSONObject) jsObject.get("XLPNRES");
+            backFrom = AdfmfJavaUtilities.evaluateELExpression("#{pageFlowScope.LpnDetailsBack}").toString();
+        } catch (NullPointerException ae) {
+            backFrom = "";
+        }
+        LpnSearchEntity[] lpnArray = null;
+        if (!backFrom.equals("FromBack")) {
+            ValueExpression ve = null;
+            s_LpnList.clear();
+            AdfmfJavaUtilities.setELValue("#{pageFlowScope.keywrdLpnLenErr}", "");
+            Utility.ApplicationLogger.info("Inside script dcomShopFloor.db");
+            String keyword = null;
+            ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.searchLpnKeyword}", String.class);
+            keyword = (String) ve.getValue(AdfmfJavaUtilities.getAdfELContext());
+            String orgCode =
+                (String) AdfmfJavaUtilities.evaluateELExpression("#{preferenceScope.feature.dcom.shop.MyWarehouse.OrgCodePG.OrgCode}");
+
+            String restURI = RestURI.PostLpnDetailsURI();
+            RestCallerUtil rcu = new RestCallerUtil();
+            String payload =
+                "{\"x\": {\"RESTHeader\": {\"@xmlns\": \"http://xmlns.oracle.com/apps/inv/rest/DCOMInquiry/header\",\"Responsibility\": \"ORDER_MGMT_SUPER_USER\",\"RespApplication\": \"ONT\",\"SecurityGroup\": \"STANDARD\",\n" +
+                "\"NLSLanguage\": \"AMERICAN\",\"Org_Id\": \"82\"},\"InputParameters\": {\"PLPNREQ\": {\"ORGCODE\": \"" +
+                orgCode + "\",\"LPN\": \"%" + keyword + "%\"}}\n" + "}\n" + "}";
+            String jsonArrayAsString = (rcu.invokeUPDATE(restURI, payload)).toString();
+
             try {
-                JSONArray array = (JSONArray) jsObject1.get("XLPNRES_ITEM");
+                JSONParser parser = new JSONParser();
+                Object object = parser.parse(jsonArrayAsString);
+                JSONObject jsonObject = (JSONObject) object;
+                JSONObject jsObject = (JSONObject) jsonObject.get("OutputParameters");
+                JSONObject jsObject1 = (JSONObject) jsObject.get("XLPNRES");
+                try {
+                    JSONArray array = (JSONArray) jsObject1.get("XLPNRES_ITEM");
 
-                if (array != null) {
-                    int size = array.size();
-                    //  ProductSearchEntity[] prodItems= new ProductSearchEntity[size];
-                    for (int i = 0; i < size; i++) {
-
-
+                    if (array != null) {
+                        int size = array.size();
+                        for (int i = 0; i < size; i++) {
+                            LpnSearchEntity lpnItems = new LpnSearchEntity();
+                            JSONObject jsObjectArrayData = (JSONObject) array.get(i);
+                            lpnItems.setORGCODE((jsObjectArrayData.get("ORGCODE").toString()));
+                            lpnItems.setLPN((jsObjectArrayData.get("LPN").toString()));
+                            lpnItems.setLPNSTATUS((jsObjectArrayData.get("LPNSTATUS").toString()));
+                            lpnItems.setSUBINV((jsObjectArrayData.get("SUBINV").toString()));
+                            lpnItems.setLOCATOR((jsObjectArrayData.get("LOCATOR").toString()));
+                            s_LpnList.add(lpnItems);
+                        }
+                    }
+                } catch (ClassCastException e2) {
+                    JSONObject jsObject2 = (JSONObject) jsObject1.get("XLPNRES_ITEM");
+                    if (jsObject2 != null) {
                         LpnSearchEntity lpnItems = new LpnSearchEntity();
-                        //  JSONObject jsObjectArrayData = (JSONObject) jsObject1.get("XLPNRES_ITEM");
-                        JSONObject jsObjectArrayData = (JSONObject) array.get(i);
-
-                        lpnItems.setORGCODE((jsObjectArrayData.get("ORGCODE").toString()));
-                        lpnItems.setLPN((jsObjectArrayData.get("LPN").toString()));
-                        lpnItems.setLPNSTATUS((jsObjectArrayData.get("LPNSTATUS").toString()));
-                        lpnItems.setSUBINV((jsObjectArrayData.get("SUBINV").toString()));
-                        lpnItems.setLOCATOR((jsObjectArrayData.get("LOCATOR").toString()));
-
-
+                        lpnItems.setORGCODE((jsObject2.get("ORGCODE").toString()));
+                        lpnItems.setLPN((jsObject2.get("LPN").toString()));
+                        lpnItems.setLPNSTATUS((jsObject2.get("LPNSTATUS").toString()));
+                        lpnItems.setSUBINV((jsObject2.get("SUBINV").toString()));
+                        lpnItems.setLOCATOR((jsObject2.get("LOCATOR").toString()));
                         s_LpnList.add(lpnItems);
                     }
-
-
                 }
-            } catch (ClassCastException e2) {
-                JSONObject jsObject2 = (JSONObject) jsObject1.get("XLPNRES_ITEM");
-                if (jsObject2 != null) {
-
-
-                    LpnSearchEntity lpnItems = new LpnSearchEntity();
-                    // JSONObject jsObject2 = (JSONObject)
-
-                    lpnItems.setORGCODE((jsObject2.get("ORGCODE").toString()));
-                    lpnItems.setLPN((jsObject2.get("LPN").toString()));
-                    lpnItems.setLPNSTATUS((jsObject2.get("LPNSTATUS").toString()));
-                    lpnItems.setSUBINV((jsObject2.get("SUBINV").toString()));
-                    lpnItems.setLOCATOR((jsObject2.get("LOCATOR").toString()));
-
-
-                    s_LpnList.add(lpnItems);
-
-
-                }
+                lpnArray = (LpnSearchEntity[]) s_LpnList.toArray(new LpnSearchEntity[s_LpnList.size()]);
+            } catch (Exception e) {
+                Trace.log("REST_JSON", Level.SEVERE, this.getClass(), "LpnSearchEntity", e.getLocalizedMessage());
             }
-
+            if (s_LpnList.size() != 0) {
+                AdfmfJavaUtilities.setELValue("#{pageFlowScope.LpnResults}", "");
+            } else {
+                AdfmfJavaUtilities.setELValue("#{pageFlowScope.LpnResults}", "No Search Results");
+            }
+        } else {
+            AdfmfJavaUtilities.setELValue("#{pageFlowScope.LpnDetailsBack}", "");
             lpnArray = (LpnSearchEntity[]) s_LpnList.toArray(new LpnSearchEntity[s_LpnList.size()]);
-            
-        
-        } catch (Exception e) {
-            Trace.log("REST_JSON", Level.SEVERE, this.getClass(), "LpnSearchEntity", e.getLocalizedMessage());
-        }
-        if(s_LpnList.size()!=0){
-            AdfmfJavaUtilities.setELValue("#{pageFlowScope.LpnResults}", "");
-        }else{
-            AdfmfJavaUtilities.setELValue("#{pageFlowScope.LpnResults}", "No Search Results");
         }
         return lpnArray;
     }
