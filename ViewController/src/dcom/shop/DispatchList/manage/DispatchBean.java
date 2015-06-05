f87b111d74c4d6bb5a790d067d59a5a072ca6b52
pradeepthewiz <pradeepthewiz@gmail.com>
2015-06-05 15:07:27 +0530

    Added a boolean variable for display purpose

--- ViewController/src/dcom/shop/DispatchList/manage/DispatchBean.java
+++ ViewController/src/dcom/shop/DispatchList/manage/DispatchBean.java
@@ -8,10 +8,27 @@ import javax.el.ValueExpression;
 import oracle.adfmf.amx.event.ValueChangeEvent;
 import oracle.adfmf.framework.api.AdfmfContainerUtilities;
 import oracle.adfmf.framework.api.AdfmfJavaUtilities;
+import oracle.adfmf.java.beans.PropertyChangeListener;
+import oracle.adfmf.java.beans.PropertyChangeSupport;
+
 
 public class DispatchBean {
+    private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
     public DispatchBean() {
         super();
+    }
+
+    boolean showAvailQty = false;
+
+    public void setShowAvailQty(boolean showAvailQty) {
+        boolean oldShowAvailQty = this.showAvailQty;
+        this.showAvailQty = showAvailQty;
+        propertyChangeSupport.firePropertyChange("showAvailQty", oldShowAvailQty, showAvailQty);
+    }
+
+    public boolean isShowAvailQty() {
+        return showAvailQty;
     }
 
     public void jobOperationsSorting(ValueChangeEvent valueChangeEvent) {
@@ -40,10 +57,47 @@ public class DispatchBean {
             AdfmfJavaUtilities.setELValue("#{pageFlowScope.trxType}", "WIP_ISSUE");
         }
     }
-    
-    public void showTrxResult(String status, String message){
-        AdfmfContainerUtilities.invokeContainerJavaScriptFunction(AdfmfJavaUtilities.getFeatureId(),
-                                                                  "showAlert", new Object[] {status,message,"Ok" });
-        
+
+    public void showTrxResult(String status, String message) {
+        AdfmfContainerUtilities.invokeContainerJavaScriptFunction(AdfmfJavaUtilities.getFeatureId(), "showAlert", new Object[] {
+                                                                  status, message, "Ok"
+        });
+
+    }
+
+    /*
+    public void fromStepChangeListener(ValueChangeEvent valueChangeEvent) {
+        // Add event code here...
+
+        if (valueChangeEvent.getNewValue().equals("Queue")) {
+        setShowAvailQty(true);
+        } else if(valueChangeEvent.getNewValue().equals("Run")) {
+            setShowAvailQty(true);
+        }
+        else {
+            setShowAvailQty(true);
+        }
+        }
+
+     */
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.removePropertyChangeListener(l);
+    }
+
+    public void fromStepListener(ValueChangeEvent valueChangeEvent) {
+        // Add event code here...
+        if (valueChangeEvent.getNewValue().equals("Queue")) {
+            setShowAvailQty(true);
+        } else if (valueChangeEvent.getNewValue().equals("Run")) {
+            setShowAvailQty(true);
+        } else {
+            setShowAvailQty(true);
+        }
+
     }
 }
